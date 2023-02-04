9772eabf3d6cbaa21dc1835896be68c372e64b12
prathamssaraf <86064024+prathamssaraf@users.noreply.github.com>
2023-02-04 16:28:33 +0530

    Update Q2.cpp

--- PP LAB/WEEK 1/Q2.cpp
+++ PP LAB/WEEK 1/Q2.cpp
@@ -1,33 +1,37 @@
 #include<iostream>
-#include<string>  
+#include<string>
+#include<ctime>
 using namespace std;
 
 int main()
 {
-	
-	int a,b,c,d;
-	int m = 0;
-	
-	cout<<"Enter the number A:";
-	cin>>a;
-	cout<<"Enter the number A:";
-	cin>>b;
-	cout<<"Enter the operation to be performed : 1 - addition , 2 - subtraction, 3 - multiplication, 4 - division";
-	cin>>c;
-	
-	if(c==1){
-		d = a+ b;
-	}else if(c == 2){
-		d = a-b;
-	}else if(c == 3){
-		d = a*b;
-	}else if(c == 4){
-		d = a/b;
-	}
-	
-	cout<<d;
-	
-	return 0;
+    clock_t start = clock();
+
+    int a,b,c,d;
+    int m = 0;
+
+    cout<<"Enter the number A:";
+    cin>>a;
+    cout<<"Enter the number B:";
+    cin>>b;
+    cout<<"Enter the operation to be performed : 1 - addition , 2 - subtraction, 3 - multiplication, 4 - division";
+    cin>>c;
+
+    if(c==1){
+        d = a+ b;
+    }else if(c == 2){
+        d = a-b;
+    }else if(c == 3){
+        d = a*b;
+    }else if(c == 4){
+        d = a/b;
+    }
+
+    cout<<d<<endl;
+
+    clock_t end = clock();
+    double time_taken = double(end - start) / CLOCKS_PER_SEC;
+    cout<<"Time taken by function: "<<time_taken<<" seconds"<<endl;
+
+    return 0;
 }
-
-
