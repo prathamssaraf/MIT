cfd2a9de93cb880345b2f3408edccb0a4d59885c
prathamssaraf <86064024+prathamssaraf@users.noreply.github.com>
2023-02-04 16:02:04 +0530

    Q1 - Week 1

--- PP LAB/WEEK 1/Q1.cpp
+++ PP LAB/WEEK 1/Q1.cpp
@@ -1,24 +1,36 @@
-#include<iostream>
-#include<string>  
+#include <iostream>
+#include <ctime>
 using namespace std;
 
-int main()
-{
-	
-	int n,i;
-	int m = 0;
-	
-	cout<<"Enter the number to be reversed:";
-	cin>>n;
-
-	while(n!=0){
-		m = m*10+ n%10;
-		n = n/10;
-	}
-
-		cout<<m;
-	
-	return 0;
+void reverseDigits(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        int number = arr[i];
+        int reverse = 0;
+        while (number != 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        arr[i] = reverse;
+    }
 }
 
+int main() {
+    int arr[9] = {18, 523, 301, 1234, 2, 14, 108, 150, 1928};
+    int size = 9;
 
+    clock_t start = clock();
+
+    reverseDigits(arr, size);
+
+    clock_t end = clock();
+    double time_taken = double(end - start) / CLOCKS_PER_SEC*1000;
+
+    cout << "Reversed Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl << "Time taken by function: " << time_taken << " seconds" << endl;
+
+    return 0;
+}
