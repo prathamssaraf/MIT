e69136bd15d2f5ea1810a1c43f7333e83a662ec0
prathamssaraf <86064024+prathamssaraf@users.noreply.github.com>
2023-02-04 18:29:43 +0530

    Update Q3.cpp

--- PP LAB/WEEK 1/Q3.cpp
+++ PP LAB/WEEK 1/Q3.cpp
@@ -1,25 +1,28 @@
-#include<iostream>
-#include<string>  
+#include <iostream>
+#include <string>  
+#include <ctime>
 using namespace std;
 
 int main()
 {
-	
-	string a;
-	cout<<"enter the string:";
-	cin>>a;	
-	for(int i=0;i<a.length();i++){
-		if(64<int(a[i])<91){
-			int p = int(a[i])+32;
-			a[i]=char(p);
-		}
-		else if(96<int(a[i])<123){
-			int p = int(a[i])-32;
-			a[i]=char(p);
-		}
-	}
-	cout<<a;
-	return 0;
+    string a;
+    cout<<"Enter the string: ";
+    getline(cin, a);
+
+    clock_t begin = clock();
+
+    for (int i = 0; i < a.length(); i++) {
+        if (isupper(a[i])) {
+            a[i] = tolower(a[i]);
+        } else if (islower(a[i])) {
+            a[i] = toupper(a[i]);
+        }
+    }
+
+    clock_t end = clock();
+    double elapsed_time = double(end - begin) / CLOCKS_PER_SEC * 1000;
+
+    cout << "Toggled string: " << a << endl;
+    cout << "Elapsed time: " << elapsed_time << "ms" << endl;
+    return 0;
 }
-
-
