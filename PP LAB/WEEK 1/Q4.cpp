ea53e029e3f8c34f40a1a82f0045fff402732223
prathamssaraf <86064024+prathamssaraf@users.noreply.github.com>
2023-02-05 23:18:25 +0530

    Update Q4.cpp

--- PP LAB/WEEK 1/Q4.cpp
+++ PP LAB/WEEK 1/Q4.cpp
@@ -1,21 +1,30 @@
-#include<iostream>
-#include<string>  
+#include <iostream>
+#include <string>
+#include <ctime>
+
 using namespace std;
 
 int main()
 {
-	
-	string a,b,c;
-	cout<<"enter the str:";
-	cin>>a;	
-	for(int i=0;i<a.length();i++){
-		for(int j=0;j<i+1;j++){
-			b= b+a[i];
-		}
-		
-	}
-	cout<<b;
-	return 0;
+    string a, b, c;
+    cout << "Enter the string: ";
+    cin >> a;
+
+    clock_t start = clock();
+    
+    for (int i = 0; i < a.length(); i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            b = b + a[i];
+        }
+    }
+    cout << b << endl;
+
+    clock_t end = clock();
+    double elapsed_time = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Elapsed time: " << elapsed_time << " seconds" << endl;
+    return 0;
 }
 
 
