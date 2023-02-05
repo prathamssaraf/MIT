ec5a513b2039ee76c0396bc3961bd6cd5de92198
prathamssaraf <86064024+prathamssaraf@users.noreply.github.com>
2023-02-06 00:05:24 +0530

    Update Q8.cpp

--- PP LAB/WEEK 1/Q8.cpp
+++ PP LAB/WEEK 1/Q8.cpp
@@ -1,62 +1,55 @@
 #include<iostream>
-#include<string>  
 #include<cmath>
+#include<ctime>
 using namespace std;
 
-int findComplementint(int n) {
-   int bit_count = floor(log2(n))+1;
-   int ones = ((1 << bit_count) - 1);
-   return ones ^ n;
-}
+// Function to find the 1's complement of an integer
 int findComplement(int n) {
-	int decimal, binary = 0, remainder, product = 1;
-	decimal = n;
-   while (decimal != 0) {
-    remainder = decimal % 2;
-    binary = binary + (remainder * product);
-    decimal = decimal / 2;
-    product *= 10;
-  }
-  return binary;
+int bit_count = floor(log2(n))+1;
+int ones = ((1 << bit_count) - 1);
+return ones ^ n;
+}
+
+int main() {
+int m, n;
+cout << "Enter the size of the matrix (MxN): ";
+cin >> m >> n;
+
+int a[m][n];
+int b[m][n];
+
+cout << "Enter the elements of matrix A: " << endl;
+for (int i = 0; i < m; i++) {
+for (int j = 0; j < n; j++) {
+cin >> a[i][j];
+}
+}
+
+for (int i = 0; i < m; i++) {
+for (int j = 0; j < n; j++) {
+b[i][j] = a[i][j];
+}
 }
 
 
-int main()
-{
-	int a[4][4]={{1,2,3,4},{6,5,8,3},{ 2,4,10,1},{9,1,2,5}};
-	int b[4][4]={{1,2,3,4},{6,5,8,3},{ 2,4,10,1},{9,1,2,5}};
-	int c[4][4]={{1,2,3,4},{6,5,8,3},{ 2,4,10,1},{9,1,2,5}};
-	
-	   
-	   for(int i=1;i<3;i++){
-	   	for(int j=1;j<3;j++){
-	   		 int n = a[i][j];
-	   		 c[i][j] = findComplementint(n);
-	   		 
-		   }
-	   }
-	   for(int i=1;i<3;i++){
-	   	for(int j=1;j<3;j++){
-	   		 int n = c[i][j];
-	   		 b[i][j] = findComplement(n);
-	   		 
-		   }
-	   }
-	   
-	   for(int i=0;i<4;i++){
-	   	for(int j=0;j<4;j++){
-	   		cout<<b[i][j]<<"  ";
-		   }
-			cout<<endl;
-	   }
-	   cout<<endl;
-	   for(int i=0;i<4;i++){
-	   	for(int j=0;j<4;j++){
-	   		cout<<c[i][j]<< "  ";
-		   }
-			cout<<endl;
-	   }
-	return 0;
+clock_t start = clock();
+for (int i = 1; i < m-1; i++) {
+for (int j = 1; j < n-1; j++) {
+b[i][j] = findComplement(b[i][j]);
+}
+}
+clock_t end = clock();
+double time = double(end-start)/CLOCKS_PER_SEC;
+
+cout << "Matrix B: " << endl;
+for (int i = 0; i < m; i++) {
+for (int j = 0; j < n; j++) {
+cout << b[i][j] << " ";
+}
+cout << endl;
 }
 
+cout << "\nTime taken by the program: " << time << "s" << endl;
+return 0;
+}
 
